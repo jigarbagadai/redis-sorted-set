ef4ff3e14170513cd1972ec8376454870c45a7fe
Jigar Bagadai <jigar.bagadai@coherent.global>
2024-03-18 16:21:34 +0530

    added sample of redis sorted set with C#

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # redis-sorted-set
-my experiement with redis storted set
+Redis Sorted Sets are similar to Sets, with unique feature of values stored in Set called scores. The score is used in order to take the sorted set ordered, from the smallest to the greatest score. Just like in Sets, members are unique but scores can be repeated. Sorted Sets are ideal for storing index data in Redis, or using some of the Sorted Set commands to find out how many users have authenticated to a system, or top users using the system etc.
+
+# redis-sorted-set with C#
+You can use the StackExchange.Redis library to work with Redis in your application. Please check program.cs file regarding how you can use a sorted set to store data with scores using C#.
---
+++ redisSortedSet/redisSortedSet/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Connect to Redis
+using StackExchange.Redis;
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("127.0.0.1:6379,ssl=false");
+IDatabase db = redis.GetDatabase();
+
+// Use current timestamp as the score
+double timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+// Add items to the sorted set with scores
+db.SortedSetAdd("mySortedSet", "item1", timestamp);
+db.SortedSetAdd("mySortedSet", "item2", timestamp + 10);
+db.SortedSetAdd("mySortedSet", "item3", timestamp + 20);
+
+// Retrieve items from the sorted set
+var items = db.SortedSetRangeByRankWithScores("mySortedSet", 0, 1, Order.Descending);
+foreach(var item in items) {
+    Console.WriteLine($"Item: {item.Element}, Score: {item.Score}");
+}
+
+// Update score of an item
+db.SortedSetRemoveRangeByRank("mySortedSet", 0, -2); // keep only 1 element
+
+//check again after remove
+var remainingElements = db.SortedSetRangeByRankWithScores("mySortedSet", 0, 1, Order.Descending);
+foreach(var item in remainingElements) {
+    Console.WriteLine($"Item: {item.Element}, Score: {item.Score}");
+}
+Console.WriteLine("Hello, World!");
